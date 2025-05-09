13f2b02b4c865dde8a12e2c5420f92b09cd52910
Lalithanjali-16 <133836180+Lalithanjali-16@users.noreply.github.com>
2025-05-09 19:59:30 +0530

    Update Display.js

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 function Display() {
